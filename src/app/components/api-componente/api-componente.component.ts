f4480d76b0bf6be937fa339a6ab7e7042266881a
Elena Rijo <alu0101265421@ull.edu.es>
2022-04-21 17:29:45 +0100

    Seguimiento de la api

--- src/app/components/api-componente/api-componente.component.ts
+++ src/app/components/api-componente/api-componente.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService, ICategoria } from 'src/app/services/api.service';
 import {
-  ServicesComponent,
   DocResult,
   Guideline,
   ApiResult,
@@ -13,7 +13,7 @@ import result from '../../services/result.json';
   styleUrls: ['./api-componente.component.css'],
 })
 export class ApiComponenteComponent implements OnInit {
-  public direccion: string;
+  public direccion: string = '';
 
   private result!: DocResult | null;
   public titulo2!: string;
@@ -22,23 +22,40 @@ export class ApiComponenteComponent implements OnInit {
   public resultadosAnalisisWeb!: ApiResult | null;
   public alerts!: any[];
   public errors!: any[];
+  public analisis: ICategoria[] = [];
 
-  constructor(private apiService: ServicesComponent) {
-    this.direccion = '';
-
-    console.log('Mi componente esta cargado');
-  }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.introducirUrl();
+    this.analizarWeb();
   }
 
-  async introducirUrl() {
-    this.resultadosAnalisisWeb = await this.apiService.consultarApi(
+  async analizarWeb() {
+    this.resultadosAnalisisWeb = await this.apiService.analizarWeb(
       this.direccion
     );
-
     if (!this.resultadosAnalisisWeb?.categories) return;
+
+    const categories = Object.values(this.resultadosAnalisisWeb.categories);
+    console.log({ categories });
+    categories.map((category) => {
+      const items = Object.values(category.items);
+    });
+    // const errorItems = Object.values(this.resultadosAnalisisWeb.categories.error.items);
+    // const errorIds = errorItems.map(error => error.id);
+
+    // const alertItems = Object.values(this.resultadosAnalisisWeb.categories.alert.items);
+    // const alertIds = errorItems.map(error => error.id);
+
+    // const errores: ICategoria = {
+    //   tipo: 'ERRORES',
+    //   pautas: [])
+    // }
+    // const alertas: ICategoria = {
+    //   tipo: 'ALERTAS',
+    //   pautas: []
+    // }
+
     this.errors = Object.values(
       this.resultadosAnalisisWeb?.categories.error.items
     );
@@ -51,7 +68,7 @@ export class ApiComponenteComponent implements OnInit {
   }
 
   async buscarID() {
-    this.result = await this.apiService.consultarID();
+    this.result = await this.apiService.consultarDoc();
     console.log(this.result);
     this.titulo2 = this.result?.title || '';
     this.guias = this.result?.guidelines || [];
